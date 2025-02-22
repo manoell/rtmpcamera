d45c0883a6796b984bb7dbad8ee82d8c72bdb490
manoell <manoel.biazotto@hotmail.com>
2025-02-22 02:55:54 -0300

    21

--- rtmp_camera_compat.m
+++ rtmp_camera_compat.m
@@ -7,6 +7,10 @@
     NSDictionary *originalProperties;
     CMTime originalFrameDuration;
     AVCaptureDeviceFormat *originalFormat;
+    AVCaptureDeviceInput *virtualInput;
+    NSString *originalDeviceID;
+    NSString *originalModelID;
+    NSString *originalLocalizedName;
 }
 
 - (instancetype)init {
@@ -22,8 +26,27 @@
     originalDevice = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
     if (!originalDevice) return;
     
-    // Store original properties
+    // Store original device identifiers
+    originalDeviceID = [originalDevice.uniqueID copy];
+    originalModelID = [originalDevice.modelID copy];
+    originalLocalizedName = [originalDevice.localizedName copy];
+    
+    // Store full device configuration
     originalProperties = @{
+        // Basic properties
+        @"uniqueID": originalDevice.uniqueID,
+        @"modelID": originalDevice.modelID,
+        @"localizedName": originalDevice.localizedName,
+        
+        // Camera capabilities
+        @"hasFlash": @(originalDevice.hasFlash),
+        @"hasTorch": @(originalDevice.hasTorch),
+        @"flashAvailable": @(originalDevice.flashAvailable),
+        @"torchAvailable": @(originalDevice.torchAvailable),
+        @"focusPointOfInterestSupported": @(originalDevice.focusPointOfInterestSupported),
+        @"exposurePointOfInterestSupported": @(originalDevice.exposurePointOfInterestSupported),
+        
+        // Current settings
         @"exposureMode": @(originalDevice.exposureMode),
         @"whiteBalanceMode": @(originalDevice.whiteBalanceMode),
         @"focusMode": @(originalDevice.focusMode),
@@ -32,114 +55,106 @@
         @"deviceWhiteBalanceGains": [NSValue valueWithCGPoint:CGPointMake(
             originalDevice.deviceWhiteBalanceGains.redGain,
             originalDevice.deviceWhiteBalanceGains.greenGain)],
-        @"lensPosition": @(originalDevice.lensPosition)
+        @"lensPosition": @(originalDevice.lensPosition),
+        @"videoZoomFactor": @(originalDevice.videoZoomFactor),
+        
+        // Ranges and limits
+        @"activeFormat": originalDevice.activeFormat,
+        @"activeVideoMinFrameDuration": [NSValue valueWithCMTime:originalDevice.activeVideoMinFrameDuration],
+        @"activeVideoMaxFrameDuration": [NSValue valueWithCMTime:originalDevice.activeVideoMaxFrameDuration],
+        @"minExposureDuration": [NSValue valueWithCMTime:originalDevice.minExposureDuration],
+        @"maxExposureDuration": [NSValue valueWithCMTime:originalDevice.maxExposureDuration],
+        @"minISO": @(originalDevice.minISO),
+        @"maxISO": @(originalDevice.maxISO),
+        @"videoZoomFactorUpscaleThreshold": @(originalDevice.videoZoomFactorUpscaleThreshold),
+        @"maxWhiteBalanceGain": @(originalDevice.maxWhiteBalanceGain),
     };
     
     originalFormat = originalDevice.activeFormat;
     originalFrameDuration = originalDevice.activeVideoMinFrameDuration;
 }
 
+- (AVCaptureDeviceInput *)createVirtualDeviceInput {
+    // Create a virtual camera device that perfectly mimics the original
+    Class virtualDeviceClass = NSClassFromString(@"AVCaptureDeviceVirtual");
+    id virtualDevice = [[virtualDeviceClass alloc] init];
+    
+    // Set all basic properties
+    [virtualDevice setValue:originalDeviceID forKey:@"uniqueID"];
+    [virtualDevice setValue:originalModelID forKey:@"modelID"];
+    [virtualDevice setValue:originalLocalizedName forKey:@"localizedName"];
+    
+    // Set capabilities
+    [virtualDevice setValue:originalProperties[@"hasFlash"] forKey:@"hasFlash"];
+    [virtualDevice setValue:originalProperties[@"hasTorch"] forKey:@"hasTorch"];
+    [virtualDevice setValue:originalProperties[@"flashAvailable"] forKey:@"flashAvailable"];
+    [virtualDevice setValue:originalProperties[@"torchAvailable"] forKey:@"torchAvailable"];
+    [virtualDevice setValue:originalProperties[@"focusPointOfInterestSupported"] forKey:@"focusPointOfInterestSupported"];
+    [virtualDevice setValue:originalProperties[@"exposurePointOfInterestSupported"] forKey:@"exposurePointOfInterestSupported"];
+    
+    // Configure format and ranges
+    [virtualDevice setValue:originalFormat forKey:@"activeFormat"];
+    [virtualDevice setValue:originalProperties[@"activeVideoMinFrameDuration"] forKey:@"activeVideoMinFrameDuration"];
+    [virtualDevice setValue:originalProperties[@"activeVideoMaxFrameDuration"] forKey:@"activeVideoMaxFrameDuration"];
+    [virtualDevice setValue:originalProperties[@"minExposureDuration"] forKey:@"minExposureDuration"];
+    [virtualDevice setValue:originalProperties[@"maxExposureDuration"] forKey:@"maxExposureDuration"];
+    [virtualDevice setValue:originalProperties[@"minISO"] forKey:@"minISO"];
+    [virtualDevice setValue:originalProperties[@"maxISO"] forKey:@"maxISO"];
+    
+    // Create input
+    NSError *error = nil;
+    virtualInput = [[AVCaptureDeviceInput alloc] initWithDevice:virtualDevice error:&error];
+    
+    if (error) {
+        NSLog(@"[RTMPCamera] Failed to create virtual device input: %@", error);
+        return nil;
+    }
+    
+    return virtualInput;
+}
+
 - (void)configureVirtualCameraWithStream:(RTMPStream *)stream {
     if (!stream) return;
     
-    // Configure stream to match original camera properties
-    [self applyOriginalPropertiesToStream:stream];
+    // Configure video format to match original camera
+    stream->video_width = (uint32_t)CMVideoFormatDescriptionGetDimensions(originalFormat.formatDescription).width;
+    stream->video_height = (uint32_t)CMVideoFormatDescriptionGetDimensions(originalFormat.formatDescription).height;
+    stream->video_fps = (uint32_t)(CMTimeGetSeconds(CMTimeMake(1, 1)) / CMTimeGetSeconds(originalFrameDuration));
     
-    // Set up format matching
-    [self setupFormatMatchingWithStream:stream];
-    
-    // Configure frame timing
-    [self configureFrameTimingWithStream:stream];
-}
-
-- (void)applyOriginalPropertiesToStream:(RTMPStream *)stream {
-    // Apply exposure settings
+    // Set camera properties
     stream->camera_properties.exposure_mode = [originalProperties[@"exposureMode"] intValue];
     stream->camera_properties.exposure_duration = [[originalProperties[@"exposureDuration"] CMTimeValue] timeValue];
     stream->camera_properties.iso = [originalProperties[@"iso"] floatValue];
-    
-    // Apply white balance
     stream->camera_properties.white_balance_mode = [originalProperties[@"whiteBalanceMode"] intValue];
+    
     CGPoint whiteBalanceGains = [[originalProperties[@"deviceWhiteBalanceGains"] CGPointValue];
     stream->camera_properties.white_balance_gains[0] = whiteBalanceGains.x;
     stream->camera_properties.white_balance_gains[1] = whiteBalanceGains.y;
     
-    // Apply focus settings
     stream->camera_properties.focus_mode = [originalProperties[@"focusMode"] intValue];
     stream->camera_properties.lens_position = [originalProperties[@"lensPosition"] floatValue];
-}
-
-- (void)setupFormatMatchingWithStream:(RTMPStream *)stream {
-    if (!originalFormat) return;
+    stream->camera_properties.zoom_factor = [originalProperties[@"videoZoomFactor"] floatValue];
     
-    CMVideoDimensions dimensions = CMVideoFormatDescriptionGetDimensions(originalFormat.formatDescription);
-    stream->video_width = dimensions.width;
-    stream->video_height = dimensions.height;
-    
-    // Match color space and pixel format
-    stream->video_color_space = originalFormat.supportedColorSpaces.firstObject;
-    stream->video_pixel_format = originalFormat.mediaType;
-    
-    // Match field of view and zoom
     if ([originalFormat respondsToSelector:@selector(videoFieldOfView)]) {
         stream->camera_properties.field_of_view = originalFormat.videoFieldOfView;
     }
-    
-    if ([originalFormat respondsToSelector:@selector(videoZoomFactor)]) {
-        stream->camera_properties.zoom_factor = originalFormat.videoZoomFactor;
-    }
-}
-
-- (void)configureFrameTimingWithStream:(RTMPStream *)stream {
-    if (CMTimeCompare(originalFrameDuration, kCMTimeZero) != 0) {
-        double fps = CMTimeGetSeconds(CMTimeMake(1, 1)) / CMTimeGetSeconds(originalFrameDuration);
-        stream->video_fps = (uint32_t)round(fps);
-    } else {
-        // Default to 30fps if we can't determine original
-        stream->video_fps = 30;
-    }
-    
-    // Configure frame delivery timing
-    stream->frame_timing.timestamp_base = CMTimeMake(1, stream->video_fps);
-    stream->frame_timing.current_timestamp = kCMTimeZero;
 }
 
 - (void)updateStreamMetadata:(RTMPStream *)stream {
     if (!stream) return;
     
     // Update device information
-    stream->device_info.model_name = [self deviceModelName];
-    stream->device_info.os_version = [self deviceOSVersion];
-    stream->device_info.unique_identifier = [self generateDeviceIdentifier];
+    stream->device_info.model_name = [originalModelID UTF8String];
+    stream->device_info.unique_identifier = [originalDeviceID UTF8String];
     
     // Update camera capabilities
-    [self updateCameraCapabilities:stream];
-}
-
-- (NSString *)deviceModelName {
-    return [[UIDevice currentDevice] model];
-}
-
-- (NSString *)deviceOSVersion {
-    return [[UIDevice currentDevice] systemVersion];
-}
-
-- (NSString *)generateDeviceIdentifier {
-    // Generate a consistent but unique identifier
-    NSString *bundleID = [[NSBundle mainBundle] bundleIdentifier];
-    NSString *deviceName = [[UIDevice currentDevice] name];
-    return [[NSString stringWithFormat:@"%@-%@", bundleID, deviceName] 
-            stringByReplacingOccurrencesOfString:@" " withString:@"_"];
-}
-
-- (void)updateCameraCapabilities:(RTMPStream *)stream {
-    stream->camera_capabilities.has_flash = originalDevice.hasFlash;
-    stream->camera_capabilities.has_torch = originalDevice.hasTorch;
-    stream->camera_capabilities.supports_focus_lock = [originalDevice isFocusModeSupported:AVCaptureFocusModeLocked];
-    stream->camera_capabilities.supports_exposure_lock = [originalDevice isExposureModeSupported:AVCaptureExposureModeLocked];
+    stream->camera_capabilities.has_flash = [originalProperties[@"hasFlash"] boolValue];
+    stream->camera_capabilities.has_torch = [originalProperties[@"hasTorch"] boolValue];
+    stream->camera_capabilities.supports_focus_lock = true;
+    stream->camera_capabilities.supports_exposure_lock = true;
     
-    // Add supported formats
-    NSArray *formats = originalDevice.formats;
+    // Get supported formats
+    NSArray *formats = [originalDevice formats];
     stream->camera_capabilities.supported_formats_count = MIN((uint32_t)formats.count, MAX_SUPPORTED_FORMATS);
     
     for (uint32_t i = 0; i < stream->camera_capabilities.supported_formats_count; i++) {
@@ -153,32 +168,10 @@
     }
 }
 
-- (void)handleCameraControl:(RTMPStream *)stream command:(CameraControlCommand)command value:(float)value {
-    switch (command) {
-        case CAMERA_CONTROL_FOCUS:
-            stream->camera_properties.lens_position = value;
-            break;
-        case CAMERA_CONTROL_EXPOSURE:
-            stream->camera_properties.exposure_duration = CMTimeMakeWithSeconds(value, 1000000);
-            break;
-        case CAMERA_CONTROL_ISO:
-            stream->camera_properties.iso = value;
-            break;
-        case CAMERA_CONTROL_ZOOM:
-            stream->camera_properties.zoom_factor = value;
-            break;
-        case CAMERA_CONTROL_WHITE_BALANCE:
-            stream->camera_properties.white_balance_gains[0] = value;
-            stream->camera_properties.white_balance_gains[1] = value;
-            break;
-        default:
-            break;
-    }
-}
-
 - (void)dealloc {
     originalProperties = nil;
     originalDevice = nil;
+    virtualInput = nil;
 }
 
 @end
